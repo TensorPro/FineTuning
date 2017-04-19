f4fa0d413fa20202083fff5702ddea36aea03c91
Akshay Mishra <lck.akshay@gmail.com>
2017-04-18 21:19:10 -0500

    Got training script working

--- train.py
+++ train.py
@@ -1,5 +1,23 @@
 from dataset import CUB200
 from torch.utils.data.dataloader import DataLoader
+from torch.utils.trainer import Trainer
 
-dataset = CUB200(".", download=True)
-loader = DataLoader(dataset, 16, 4)
+from torch import nn, optim
+from torch.autograd import Variable
+from torchvision.models import resnet18
+import torch.nn.functional as F
+
+NUM_EPOCHS = 1
+BATCH_SIZE=2
+
+dataset = CUB200(".", download=False, nb_examples=100)
+loader = DataLoader(dataset, BATCH_SIZE)
+
+model = resnet18()
+model.fc = nn.Linear(512,200)
+criterion = nn.CrossEntropyLoss()
+
+optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+trainer = Trainer(model, criterion, optimizer, loader)
+trainer.run(NUM_EPOCHS)
